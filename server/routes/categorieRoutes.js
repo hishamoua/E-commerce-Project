f40d363abee0decdf94c43a7c61f6c81226adfe8
hishamoua <125453240+hishamoua@users.noreply.github.com>
2023-10-20 11:26:54 +0100

    changes to the categorieRoutes

--- server/routes/categorieRoutes.js
+++ server/routes/categorieRoutes.js
@@ -11,6 +11,15 @@ router.get('/', async (req, res) =>{
     res.send(categoryList);
 })
 
+router.get('/:id', async(req, res)=>{
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found!.'})
+    }
+    res.status(200).send(category);
+})
+
 router.post('/', async (req,res)=>{
         const category = await Category.create({
         name: req.body.name,
@@ -25,6 +34,22 @@ router.post('/', async (req,res)=>{
     res.send(category);
 })
 
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        { new: true}
+        )
+    category = await category.save();
+    
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+    res.send(category);
+})
 router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category =>{
         if(category) {
@@ -37,4 +62,4 @@ router.delete('/:id', (req, res)=>{
     })
 })
 
-module.exports =router;
+module.exports = router;
